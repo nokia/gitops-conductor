3927ac473cf38cbfbfd00c92d68381ae5eba742a
Niklas Wik <niklas.wik@nokia.com>
2019-04-04 14:20:36 +0300

    Add template possibilty for get k8s node amount
    Signed-off-by: Niklas Wik <niklas.wik@nokia.com>

--- pkg/template/additional.go
+++ pkg/template/additional.go
@@ -7,6 +7,9 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func templateFuncs(baseDir string) template.FuncMap {
@@ -18,6 +21,22 @@ func templateFuncs(baseDir string) template.FuncMap {
 			return "", err
 		}
 		return string(data), nil
+	}
+	m["k8sNodes"] = func() (int, error) {
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			return 0, err
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			return 0, err
+		}
+		nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
+		if err != nil {
+			return 0, err
+		}
+		return len(nodes.Items), nil
 	}
 	return m
 
